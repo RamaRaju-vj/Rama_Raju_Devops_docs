d479e4f7b002198d3e49d3eb7f59b63ba16854e1
RamaRaju-vj <vadapalli.j@northeastern.edu>
2026-02-11 19:02:40 -0500

    docs: Update release configuration for semantic-release plugins

--- release.config.js
+++ release.config.js
@@ -1,19 +1,22 @@
 module.exports = {
   branches: ['main'],
+  tagFormat: 'v${version}',
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
     [
-      '@semantic-release/changelog',
+      '@semantic-release/commit-analyzer',
       {
-        changelogFile: 'CHANGELOG.md',
+        preset: 'conventionalcommits',
+        releaseRules: [{ type: 'docs', release: 'patch' }],
       },
     ],
+    '@semantic-release/release-notes-generator',
+    ['@semantic-release/changelog', { changelogFile: 'CHANGELOG.md' }],
     [
       '@semantic-release/git',
       {
-        assets: ['CHANGELOG.md', 'package.json'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+        assets: ['CHANGELOG.md'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
     ],
     '@semantic-release/github',
